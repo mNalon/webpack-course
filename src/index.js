a5819962e85fa3e32555162ebfa44a6ad58a820b
mnalon <marcelo.nalon@engenharia.ufjf.br>
2022-06-13 23:08:13 -0300

    Load and inject css style with sass-loader

--- src/index.js
+++ src/index.js
@@ -5,8 +5,12 @@ import fooBarImgJPEG from './foo-bar.jpeg'
 import fooBarImgPNG from './foo-bar.png'
 
 import AddRandomTextButton from './components/add-random-text-button/index.js'
+import { PageHeader } from './components/page-header/index.js'
 
 helloWorld();
+
+document.body.appendChild(PageHeader('Webpack 5 course!'))
+
 appendImage(fooBarImgJPEG);
 appendImage(fooBarImgPNG);
 
